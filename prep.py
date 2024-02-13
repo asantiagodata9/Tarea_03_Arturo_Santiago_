8072ed893f2e6f13408bbbc8dbfd80195bcfb57d
asantiagodata9 <157444014+asantiagodata9@users.noreply.github.com>
2024-02-12 23:47:43 -0600

    Update prep.py

--- prep.py
+++ prep.py
@@ -1,8 +1,4 @@
-
 from src.data_preparation import prepare_data
 
 if __name__ == "__main__":
-    train_features, test_features, train_labels = prepare_data('data/raw/train.csv', 'data/raw/test.csv')
-    train_features.to_csv('data/prep/train_features.csv', index=False)
-    test_features.to_csv('data/prep/test_features.csv', index=False)
-    train_labels.to_csv('data/prep/train_labels.csv', index=False)
+    prepare_data('data/raw/train.csv', 'data/raw/test.csv')
