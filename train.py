db0eaed025b06d8c7f5014b0b825b799af32cb14
asantiagodata9 <157444014+asantiagodata9@users.noreply.github.com>
2024-02-13 18:01:29 -0600

    Update train.py

--- train.py
+++ train.py
@@ -2,8 +2,12 @@
 from src.model_training import train_model
 
 if __name__ == "__main__":
-    cat_features = ['a list of categorical feature names goes here']  # Update this list with actual categorical feature names
-    train_model('data/prep/train_features.csv', 'data/prep/train_labels.csv', cat_features, 'model.joblib')
+    # Update this list with actual categorical feature names
+    cat_features = ['a list of categorical feature names goes here']
+    train_model('data/prep/train_features.csv',
+                'data/prep/train_labels.csv',
+                cat_features,
+                'model.joblib')
 
     # Guardar el modelo entrenado
     joblib.dump(model, MODEL_PATH)
