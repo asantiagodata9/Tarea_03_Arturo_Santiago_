9d91ba7070693f24edc01738fb44a866b1a347e2
asantiago9 <arturo.santiago9@gmail.com>
2024-02-18 18:17:14 -0600

    Update train.py

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ TRAIN_DATA_PATH = 'data/train.csv'
 MODEL_PATH = 'data/model/model.joblib'  # Asegurarse de que corresponda a model.joblib
 
 # Cargar el archivo de configuración YAML
-with open('config.yml', 'r') as file:
+with open('config.yml', 'r', encoding='utf-8') as file:
     config = yaml.safe_load(file)
 
 # Extraer los hiperparámetros para el modelo CatBoostRegressor
@@ -36,4 +36,4 @@ train_features = train_data.iloc[:len(train_labels), :]
 cat_features = get_categorical_features(train_features)
 
 # Entrenar el modelo y guardar en la ruta especificada
-train_model(train_features, train_labels, cat_features, model_path=MODEL_PATH)
+train_model(train_features, train_labels, cat_features, model_params, model_path=MODEL_PATH)
