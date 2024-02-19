8a678523163ff910509f0946d3367fb0726ce64b
asantiago9 <arturo.santiago9@gmail.com>
2024-02-18 20:15:58 -0600

    Update train.py

--- train.py
+++ train.py
@@ -7,24 +7,27 @@ def main():
     Script principal para entrenar un modelo de CatBoostRegressor.
     
     Este script permite entrenar un modelo utilizando datos preparados y etiquetas de entrenamiento.
-    Utiliza un archivo de configuración YAML para los hiperparámetros del modelo y permite la especificación
-    de rutas a través de argumentos de línea de comandos para los datos preparados, datos de entrenamiento,
-    y la ubicación para guardar el modelo entrenado.
+    Utiliza un archivo de configuración YAML para los hiperparámetros del modelo
+    y permite la especificación
+    de rutas a través de argumentos de línea de comandos para los datos preparados,
+    datos de entrenamiento, y la ubicación para guardar el modelo entrenado.
     
     Ejemplo de uso:
         python train.py --prepared_data_path data/prep/all_features.csv
                         --train_data_path data/train.csv
                         --model_path data/model/model.joblib
     """
-    
+
     # Configura el analizador de argumentos para aceptar rutas de archivos y directorios
     parser = argparse.ArgumentParser(description="Entrena un modelo de CatBoostRegressor.")
     parser.add_argument('--config_path', type=str, default='config.yml',
                         help='Ruta del archivo de configuración YAML.')
     parser.add_argument('--prepared_data_path', type=str, required=True,
                         help='Ruta del archivo CSV que contiene los datos preparados.')
-    parser.add_argument('--train_data_path', type=str, required=True,
-                        help='Ruta del archivo CSV que contiene los datos de entrenamiento con etiquetas.')
+    parser.add_argument('--train_data_path',
+                        type=str,
+                        required=True,
+                        help='Ruta archivo CSV que contiene los datos de entrenamiento con etiquetas.')
     parser.add_argument('--model_path', type=str, required=True,
                         help='Ruta donde se guardará el modelo entrenado.')
 
@@ -45,7 +48,11 @@ def main():
 
     # Entrenar el modelo con los datos, hiperparámetros, y características categóricas especificadas
     # Guardar el modelo entrenado en la ruta especificada
-    train_model(train_features, train_labels, cat_features, model_params, model_path=args.model_path)
+    train_model(train_features,
+                train_labels,
+                cat_features,
+                model_params,
+                model_path=args.model_path)
 
 if __name__ == '__main__':
     main()
