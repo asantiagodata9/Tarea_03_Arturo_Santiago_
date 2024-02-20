bf2c4a4333d66b417ea7438f3bda23f78d4802bd
asantiago9 <arturo.santiago9@gmail.com>
2024-02-19 23:17:18 -0600

    Update prep.py

--- prep.py
+++ prep.py
@@ -1,30 +1,35 @@
-"""Módulo de preparación de datos para análisis o modelado de aprendizaje automático.
+"""
+Módulo de preparación de datos para análisis o modelado de aprendizaje automático.
 
 Este script utiliza la función 'prepare_data' del módulo 'data_preparation'
-para preparar datos crudos.
-Los usuarios pueden especificar las rutas de entrada para los archivos
-de datos de entrenamiento y prueba,
-y la ruta de salida para guardar los datos procesados.
+para preparar datos crudos. Los usuarios pueden especificar las rutas de entrada
+para los archivos de datos de entrenamiento y prueba, y la ruta de salida para
+guardar los datos procesados.
 
 Ejemplo de uso:
     $ python prep.py 
-    --train_path <ruta_al_archivo_de_entrenamiento> 
-    --test_path <ruta_al_archivo_de_prueba> 
+    --train_path <ruta_al_archivo_de_entrenamiento>
+    --test_path <ruta_al_archivo_de_prueba>
     --output_path <ruta_para_guardar_datos_procesados>
 """
 
 import argparse
+import logging
 from data_preparation import prepare_data
 from src.log_config import configure_logging
 
+# Configuración de logging para este script
 configure_logging('prep')
 
 def main():
-    """Ejecuta la preparación de datos basándose en los argumentos
-    de línea de comando proporcionados."""
+    """
+    Ejecuta la preparación de datos basándose en los argumentos
+    de línea de comando proporcionados.
+    """
+    # Configuración del analizador de argumentos
     parser = argparse.ArgumentParser(
-     description='Preparar datos para análisis o modelado de aprendizaje automático.'
-     )
+        description='Preparar datos para análisis o modelado de aprendizaje automático.'
+    )
     parser.add_argument('--train_path', type=str, required=True, help=
                         'Ruta del archivo de datos de entrenamiento crudos.')
     parser.add_argument('--test_path', type=str, required=True, help=
@@ -34,8 +39,17 @@ def main():
 
     args = parser.parse_args()
 
-    # Llamar a la función prepare_data para preparar los datos
-    prepare_data(train_path=args.train_path, test_path=args.test_path, output_path=args.output_path)
+    # Inicio del proceso de preparación de datos
+    logging.info(f'Iniciando la preparación de datos desde {args.train_path} y {args.test_path}.')
+
+    try:
+        # Llamar a la función prepare_data para preparar los datos
+        prepare_data(train_path=args.train_path, test_path=args.test_path, output_path=args.output_path)
+        logging.info(f'Datos procesados guardados correctamente en {args.output_path}.')
+    except Exception as e:
+        logging.error('Ocurrió un error durante la preparación de datos: %s', e)
+        raise
 
 if __name__ == '__main__':
     main()
+    
