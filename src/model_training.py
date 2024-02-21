51327625f42e72f6d3021342f71aef13abf5539f
asantiago9 <arturo.santiago9@gmail.com>
2024-02-20 22:38:10 -0600

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -14,6 +14,7 @@ Obtiene las características categóricas de un DataFrame.
 import pandas as pd
 from catboost import CatBoostRegressor
 import joblib  # Importar joblib
+import logging
 
 def load_prepared_data(path):
     """
@@ -25,7 +26,11 @@ def load_prepared_data(path):
     Retorna:
     - pandas.DataFrame: DataFrame que contiene los datos preparados.
     """
-    return pd.read_csv(path)
+    data = pd.read_csv(path)
+    logging.debug("Datos cargados de %s con %d filas y %d columnas.",
+                  path, data.shape[0],
+                  data.shape[1])
+    return data
 
 def train_model(train_features,
                 train_labels,
@@ -50,10 +55,15 @@ def train_model(train_features,
     Retorna:
     - None
     """
+
+    logging.debug("Comenzando entrenamiento del modelo con %d características y %d ejemplos.",
+                  train_features.shape[1],
+                  train_features.shape[0])
     model = CatBoostRegressor(**model_params)
     model.fit(train_features, train_labels, cat_features=cat_features)
     joblib.dump(model, model_path)  # Guardar el modelo con joblib
-    print("Model trained and saved at:", model_path)
+    logging.debug("Modelo entrenado y guardado en %s.", model_path)
+    print("Modelo entrenado y guardado en:", model_path)
 
 def get_categorical_features(train_features):
     """
@@ -65,4 +75,6 @@ def get_categorical_features(train_features):
     Retorna:
     - list: Lista de nombres de características categóricas.
     """
-    return [col for col in train_features.columns if train_features[col].dtype == 'object']
+    cat_features = [col for col in train_features.columns if train_features[col].dtype == 'object']
+    logging.debug("Identificadas %d características categóricas.", len(cat_features))
+    return cat_features
